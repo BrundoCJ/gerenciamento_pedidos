e0f3751dffed175fb2e712530d73083e6ea0a737
BrundoCJ <bconrado04@gmail.com>
2026-03-24 01:50:02 -0300

    adicionando service

---
+++ demo/src/main/java/com/gerenciamento_de_pedidos/demo/service/PedidoService.java
@@ -0,0 +1,38 @@
+package com.gerenciamento_de_pedidos.demo.service;
+
+import com.gerenciamento_de_pedidos.demo.entity.Pedido;
+import com.gerenciamento_de_pedidos.demo.repository.PedidoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+
+    private final PedidoRepository pedidoRepository;
+
+    public PedidoService(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    public Pedido salvar(Pedido pedido) {
+        return pedidoRepository.save(pedido);
+    }
+
+    public List<Pedido> listarTodos() {
+        return pedidoRepository.findAll();
+    }
+
+    public Optional<Pedido> buscarPorId(Long id) {
+        return pedidoRepository.findById(id);
+    }
+
+    public boolean deletar(Long id) {
+        if (pedidoRepository.existsById(id)) {
+            pedidoRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
